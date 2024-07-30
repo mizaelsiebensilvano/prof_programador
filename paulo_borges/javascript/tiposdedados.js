62d7c7ce44c1f37c6de4fa9a710e09d070a5bd0b
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-07-30 01:09:01 -0300

    update aprendendo JS com Paulo Borges

---
+++ paulo_borges/javascript/tiposdedados.js
@@ -0,0 +1,30 @@
+
+/* 
+TIPOS DE DADOS são ferramentas para construirmos os nossos algorítmos.
+STRING é um bloco de texto dentro de aspas.
+TEMPLATE STRING serve para colocar variáveis e valores usando ${}.
+NUMBER é um número, se envolver com aspas ele muda para string.
+TYPEOF serve para saber qual o tipo de dados.
+NULL é um valor nulo. Ele existe mas é um valor nulo. Tem ausência de valor.
+INDEFINED é um valor indefinido. Ele simplesmente não existe.
+No Javascript todo tipo de dado é um OBJETO.
+BOOLEAN é um verdadeiro ou falso.
+Adotam o valor de FALSE o FALSE, 0, NaN, NULL, UNDEFINED, '' (string sem valor)
+Adontam o valor de TRUE ' '(string com caracteres), qualquer número diferente de 0,
+*/
+
+
+const nossaString = 'Mizael é'
+const outraString = 'Programador'
+const numero = 3
+const valorDaCompra = null
+// const undefined = undefined
+let valor
+
+const boolean = false
+
+const formado = false
+
+const faltosos = NaN
+
+console.log()
---
+++ paulo_borges/javascript/variaveis.js
@@ -0,0 +1,19 @@
+/* alert('Agora é um alerta!') 
+    Comando ALERT é uma ferramenta do Javascript para executar funções através de uma aba.
+    Comando CONSOLE é uma ferramendo do Javascript para executar funções.
+*/  
+
+/* VARIÁVES servem para guardar dados em memória
+    Existem 3 tipos de variáveis: VAR, LET e CONST
+    VAR e LET consigo redeclarar, a diferença é só de escopo
+    CONST não consigo redeclarar
+    As variáveis servem pra vc não ficar repetindo o mesmo valor muitas vezes.
+    Uma variável nunca pode iniciar por número, pode iniciar por letra, anderline ou sifrão.
+    Os númeres podem estar no meio ou no final da variável.
+    É comum utilizarmos um padrão de escrita camelCase.
+*/
+
+// let fraseQualquerDepois = 'Essa é uma frase'  
+//  fraseQualquerDepois = 'outro valor'
+
+// const outraFrase = 'frase'
