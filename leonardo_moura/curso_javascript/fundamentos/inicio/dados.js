d1ef27c28f2857bc352a2814d529c49d9be340eb
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-08-10 10:50:37 -0300

    update Curso JS Leo Moura

--- leonardo_moura/curso_javascript/fundamentos/inicio/dados.js
+++ leonardo_moura/curso_javascript/fundamentos/inicio/dados.js
@@ -16,17 +16,44 @@
 
 // console.log(19.9 * 0.6)
 
-let preco = 19.90
-let desconto = 0.40
-let precoComDesconto = preco * (1 - desconto)
-// console.log(precoComDesconto)
+// let preco = 19.90
+// let desconto = 0.40
+// let precoComDesconto = preco * (1 - desconto)
+// // console.log(precoComDesconto)
 
-let nome = "Caderno "
-let categoria = "Papelaria"
-console.log("Produto: " + nome
-    + "Categoria: " + categoria
-    + ", Preco: " + preco
-    + ", Desconto: " + desconto)
+// let nome = "Caderno "
+// let categoria = "Papelaria"
+// console.log("Produto: " + nome
+//     + "Categoria: " + categoria
+//     + ", Preco: " + preco
+//     + ", Desconto: " + desconto)
 
-    console.log(1 + 1)
-    console.log("1" + 1)
+//     console.log(1 + 1)
+//     console.log("1" + 1)
+
+// let idade = 31
+// console.log(typeof 31)      // number
+// console.log(typeof idade)   // number
+
+// let salario = 457.832
+// console.log(typeof salario) // number
+
+// let estaChovendo = true // ou false 
+// console.log(typeof estaChovendo) // boolean
+
+// console.log(typeof "Teste") // string
+// console.log(typeof 'Teste') // string
+
+/*
+    number = número
+    boolean = true or false = verdadeiro ou falso
+    string = é uma sequência de caracteres
+
+    NUMBER, BOOLEAN E STRING = esses são os 3 tipos básicos suportados pelo Javascript
+*/
+
+// const a = 3
+
+// // a = 10
+
+// console.log(a)
---
+++ leonardo_moura/curso_javascript/fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+const PI = 3.141592
+let raio = 10
+let areaCirc = PI * raio * raio
+
+areaCirc = Math.PI * raio * raio
+
+console.log(PI)
+console.log(raio)
+console.log(`O valor da área é ` + areaCirc + ` m2.`)
+console.log(Math.PI)
+console.log('O valor da área é ' + areaCirc + ' m2.')
