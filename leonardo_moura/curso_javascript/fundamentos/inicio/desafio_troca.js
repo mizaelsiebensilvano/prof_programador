7585ea35af12293688d562943debf28327521bc9
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-08-12 21:23:00 -0300

    curso JS Leo Moura

---
+++ leonardo_moura/curso_javascript/fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,17 @@
+// /*
+// DESAFIO TROCA
+// let a = 7
+// let b = 94
+
+// depois da troca... a = 94 e b = 7
+// */
+
+// let a = 7
+// let b = 94
+
+// let temp = a
+// a = b
+// b = temp
+
+// console.log(a)
+// console.log(b)
