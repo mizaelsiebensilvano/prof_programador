82dc5b1ad4f45d4d1bf75c67c0244794ec23ff62
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-08-16 20:06:37 -0300

    curso JS com Leo Moura

--- leonardo_moura/curso_javascript/fundamentos/inicio/exercicios_js/fundamentos/funcao2.js
+++ leonardo_moura/curso_javascript/fundamentos/inicio/exercicios_js/fundamentos/funcao2.js
@@ -1,20 +1,20 @@
-// Armazenando uma função em uma variável
-const imprimirSoma = function (a, b) {
-    console.log(a + b)
-}
+// // Armazenando uma função em uma variável
+// const imprimirSoma = function (a, b) {
+//     console.log(a + b)
+// }
 
-imprimirSoma(2, 3)
+// imprimirSoma(2, 3)
 
-// Armazenando uma função arrow em uma variável
-const soma = (a, b) => {
-    return a + b
-}
+// // Armazenando uma função arrow em uma variável
+// const soma = (a, b) => {
+//     return a + b
+// }
 
-console.log(soma(2, 3))
+// console.log(soma(2, 3))
 
-// retorno implícito
-const subtracao = (a, b) => a - b
-console.log(subtracao(2, 3))
+// // retorno implícito
+// const subtracao = (a, b) => a - b
+// console.log(subtracao(2, 3))
 
-const imprimir2 = a => console.log(a)
-imprimir2('Legal!!!')
+// const imprimir2 = a => console.log(a)
+// imprimir2('Legal!!!')
