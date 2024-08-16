71ea141136bd6066cb85e6b0d20805b8af068229
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-08-16 02:30:11 -0300

    curso JS com Leo Moura

--- leonardo_moura/curso_javascript/fundamentos/inicio/exercicios_js/fundamentos/objeto.js
+++ leonardo_moura/curso_javascript/fundamentos/inicio/exercicios_js/fundamentos/objeto.js
@@ -1,13 +1,13 @@
-const prod1 = {}
-prod1.nome = 'Celular Ultra Mega'
-prod1.preco = 4998.90  
-prod1['desconto Legal'] = 0.40 // evitar atributos com espaço
+// const prod1 = {}
+// prod1.nome = 'Celular Ultra Mega'
+// prod1.preco = 4998.90  
+// prod1['desconto Legal'] = 0.40 // evitar atributos com espaço
 
-console.log(prod1)
+// console.log(prod1)
 
-const prod2 = {
-    nome: 'Camisa Polo',
-    preco: 79.90,
-}
+// const prod2 = {
+//     nome: 'Camisa Polo',
+//     preco: 79.90,
+// }
 
-console.log(prod2)
+// console.log(prod2)
