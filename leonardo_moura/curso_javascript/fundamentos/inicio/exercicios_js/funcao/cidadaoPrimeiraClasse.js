0a941011e361bed6e55f74acf1b8af9df6206950
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-08-24 09:32:50 -0300

    curso JS com Leo Moura
    atualizando

---
+++ leonardo_moura/curso_javascript/fundamentos/inicio/exercicios_js/funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,19 @@
+/*
+    Função em JS é First-Class Object (Citizens)
+    Higher-order function
+*/
+
+// Criar de forma literal
+function fun1() {}
+
+// Armazenar em uma variável
+const fun2 = function () { }
+
+// Armazenar em um array
+const array = [function (a, b) { return a + b}, fun1, fun2]
+console.log(array[0](2, 3))
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () { return 'Opa'}
+console.log(obj.falar()) 
