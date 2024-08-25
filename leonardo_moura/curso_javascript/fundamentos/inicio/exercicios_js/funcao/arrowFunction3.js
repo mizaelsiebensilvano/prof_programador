278c2a87d620e8ae3e45137b699899209183da58
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-08-25 17:18:49 -0300

    curso de JS com Leo Moura
    Funções Callback

---
+++ leonardo_moura/curso_javascript/fundamentos/inicio/exercicios_js/funcao/arrowFunction3.js
@@ -0,0 +1,18 @@
+// let comparaComThis = function (param) {
+//     console.log(this === param)
+// }
+
+// comparaComThis()
+
+// const obj = {}
+// comparaComThis = comparaComThis.bind(obj)
+// comparaComThis()
+// comparaComThis(obj)
+
+// let comparaComThisArrow = param => console.log(this === param)
+// comparaComThis()
+// comparaComThis(module.exports)
+
+// comparaComThisArrow = comparaComThisArrow.bind(obj)
+// comparaComThisArrow(obj)
+// comparaComThisArrow(module.exports)
