20d6d338e538b0201d5506ef2a374010907258a2
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-08-26 18:54:58 -0300

    curso de JS com Leo Moura
    Tipos de Declaração

--- leonardo_moura/curso_javascript/fundamentos/inicio/exercicios_js/funcao/funcoesConstrutora.js
+++ leonardo_moura/curso_javascript/fundamentos/inicio/exercicios_js/funcao/funcoesConstrutora.js
@@ -1,29 +1,29 @@
-function Carro(velocidadeMaxima = 200, delta = 5) {
-    // Atributo Privado
-    let velocidadeAtual = 0
+// function Carro(velocidadeMaxima = 200, delta = 5) {
+//     // Atributo Privado
+//     let velocidadeAtual = 0
 
-    // Método Público
-    this.acelerar = function () {
-        if (velocidadeAtual + delta <= velocidadeMaxima) {
-            velocidadeAtual += delta
-        } else {
-            velocidadeAtual = velocidadeMaxima
-        }
-    }
+//     // Método Público
+//     this.acelerar = function () {
+//         if (velocidadeAtual + delta <= velocidadeMaxima) {
+//             velocidadeAtual += delta
+//         } else {
+//             velocidadeAtual = velocidadeMaxima
+//         }
+//     }
 
-    // Método Público
-    this.getVelocidadeAtual = function () {
-        return velocidadeAtual
-    }
-}
+//     // Método Público
+//     this.getVelocidadeAtual = function () {
+//         return velocidadeAtual
+//     }
+// }
 
-const uno = new Carro
-uno.acelerar()
-console.log(uno.getVelocidadeAtual())
+// const uno = new Carro
+// uno.acelerar()
+// console.log(uno.getVelocidadeAtual())
 
-const ferrari = new Carro(350, 20)
-ferrari.acelerar()
-console.log(ferrari.getVelocidadeAtual())
+// const ferrari = new Carro(350, 20)
+// ferrari.acelerar()
+// console.log(ferrari.getVelocidadeAtual())
 
-console.log(typeof Carro)
-console.log(typeof ferrari)
+// console.log(typeof Carro)
+// console.log(typeof ferrari)
