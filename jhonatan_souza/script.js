299796b51c49b913dfc274d2b4f691d52129d247
mizaelsiebensilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-09-05 17:56:22 -0300

    exercicio JS
    Jhonatan Souza

---
+++ jhonatan_souza/script.js
@@ -0,0 +1,76 @@
+// let idade = 5
+// console.log(idade)
+// let altura = 180
+// console.log(altura)
+// let nome = 'mizael'
+// console.log(nome)
+// let nome = 'Mizael'
+// let idade = '45'
+// let peso = 80
+// console.log(nome)
+// console.log(idade)
+// console.log(peso)
+
+// const valorIngressoAdulto = 20
+// // valorIngressoAdulto = 30
+// console.log(valorIngressoAdulto)
+
+// let nome = 'Mizael' // string literal
+// let idade = 45  // number literal
+// let estaAprovado = true // boolean
+// let sobrenome = undefined  // undefined
+// let corSelecionada = null // redefinir um valor
+
+// let pessoa = {
+//     nome: 'Mizael',
+//     idade: 45,
+//     estaAprovado: true,
+//     sobrenome: 'Sieben Silvano',
+//     endereco: 'Rua Vereador Carlos Hirakawa, 68, Miracatu-SP',
+//     peso: '80 kg'
+// }
+
+// console.log(pessoa)
+
+// let familia = [true,45,'mizael',1978]
+// console.log(familia.length)
+// console.log(familia[0])
+
+// let nomeDoColega = ['Diego',30,'Miracatu-SP']
+// console.log(nomeDoColega)
+// console.log(nomeDoColega.length)
+
+// função (verbo + substantivo = resetaCor)
+// let corSite = "azul"
+// function resetaCor(cor, tonalidade){
+//     corSite = cor + ' ' + tonalidade
+// }
+
+// console.log(corSite)
+// resetaCor("verde", "claro")
+// console.log(corSite)
+
+// let marcaCarro = "Peugeot"
+// function caracteristicaVeiculo(marca, modelo){
+//     marcaCarro = marca + ' ' + modelo
+// }
+
+// console.log(marcaCarro)
+// caracteristicaVeiculo("Peugeot", "408")
+// console.log(marcaCarro)
+
+// let geladeira = 'legumes'
+// function temNaCozinha(legumes, proteina){
+//     geladeira = legumes + ' ' + proteina
+// }
+// console.log(geladeira)
+// temNaCozinha('legumes', 'carne')
+// console.log(geladeira)
+
+// let mesa = 'notebook'
+// function temNaMesa(notebook, mouse) {
+//     mesa = notebook + ' ' + mouse
+// }
+// console.log(mesa)
+// temNaMesa('notebook', 'mouse')
+// console.log(mesa)
